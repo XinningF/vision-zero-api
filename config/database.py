08d674cdd40e2921ff10a72b67138252ee38d03a
Esther Fang <xf49@cornell.edu>
2022-10-20 00:18:48 -0400

    Update database.py

--- config/database.py
+++ config/database.py
@@ -11,17 +11,15 @@ intersection_dangerousity = db["intersect_dangerousity_1_0"]
 print("connected to mongodb")
 
 # get lat, long
+# for record in intersection_dangerousity.find():
+#     coord_str = record["coordinates"][1:-1]
+#     coord_str_list = coord_str.split(",")
+#     lat, long = float(coord_str_list[0]), float(coord_str_list[1])
 
+#     myquery = { "_id": record["_id"] }
+#     newvalue = { "$set": { "loc": [long, lat] } }
 
-for record in intersection_dangerousity.find():
-    coord_str = record["coordinates"][1:-1]
-    coord_str_list = coord_str.split(",")
-    lat, long = float(coord_str_list[0]), float(coord_str_list[1])
+#     intersection_dangerousity.update_one(myquery, newvalue)
+#     print(record["_id"])
 
-    myquery = { "_id": record["_id"] }
-    newvalue = { "$set": { "loc": [long, lat] } }
-
-    intersection_dangerousity.update_one(myquery, newvalue)
-    print(record["_id"])
-
-print("success")
+# print("success")
